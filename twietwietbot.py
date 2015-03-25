9e8cbb1b66c7cfc0635ad7e943a737c29aa0fdc7
Roy D <r.a.david@student.rug.nl>
2015-03-25 22:08:40 +0100

    updated taakverdeling, twietwietbot

--- twietwietbot.py
+++ twietwietbot.py
@@ -3,8 +3,8 @@
 
 import tweepy, time, sys, twietwiets
 
-CONSUMER_KEY = 'NkO6DvRBtf6LMg4uKS5ip8ly0'
-CONSUMER_SECRET = 'ER9uOTsYkkjF7AntiFGJQn9GrowaR133F2Uik1h4xMTv23Y7Ll'
+CONSUMER_KEY = 'XPkqjxqIOx8IKifTWPnEi8coU'
+CONSUMER_SECRET = 'cBQeVEcNOUlztwyDVnqo4y7nBvvdm3s0iMZ83NBXIroqWeIkcF'
 ACCESS_KEY = '3115005287-qmh9xd4wXXqftCxyf1BHW7oRvQHcUKDwTmU6BCo'
 ACCESS_SECRET = 'JSn387CHzXcN99iBvXbADGvR51pTQpmYD2w2Rvz9gDydZ'
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -15,12 +15,20 @@ api = tweepy.API(auth)
 twieTweet = twietwiets.twietwiets()
 while 1 > 0:
     twietwiet = twieTweet.get_twietwiet()
-    print(twietwiet)
+    """print kun je behouden als je wilt zien
+    wat er getweet wordt zonder op twitter te kijken"""
     line1 = " ".join(twietwiet[0])
     line2 = " ".join(twietwiet[1])
     tweet = line1 + '\n' + line2
-    if len(line1) <= 128 and len(line2) <= 128:
+    print(tweet)
+    if len(tweet) <= 140:
         api.update_status(status=tweet)
         time.sleep(3600)
+    else:
+        twietwiet = twieTweet.get_twietwiet()
+        line1 = " ".join(twietwiet[0])
+        line2 = " ".join(twietwiet[1])
+        tweet = line1 + '\n' + line2
+        print(tweet) 
 
 """used this tutorial: http://www.dototot.com/how-to-write-a-twitter-bot-with-python-and-tweepy/"""
